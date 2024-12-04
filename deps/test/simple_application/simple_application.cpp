5f67eef43c71a5687a0b286e4e5fd9ed293e2fa3
Dm1stry <BattleshipWriter@yandex.ru>
2024-12-04 21:24:38 +0300

    Unneccessary changes

--- deps/test/simple_application/simple_application.cpp
+++ deps/test/simple_application/simple_application.cpp
@@ -3,25 +3,22 @@
 #include <MRViewer/MRSetupViewer.h>
 #include <MRViewer/MRViewer.h>
 
-//#include "Pipeline.h"
-
-int main( int argc, char** argv )
+int main(int argc, char **argv)
 {
-    MR::setupLoggerByDefault();
+	MR::setupLoggerByDefault();
 
-    // Init the viewer
+	// Init the viewer
 
-    MR::Viewer::LaunchParams launchParams{ .argc = argc, .argv = argv };
-    MR::Viewer::parseLaunchParams( launchParams );
-    launchParams.name = "DMD application";
+	MR::Viewer::LaunchParams launchParams{.argc = argc, .argv = argv};
+	MR::Viewer::parseLaunchParams(launchParams);
+	launchParams.name = "DMD application";
 
-    MR::ViewerSetup viewerSetup;
-    viewerSetup.setupBasePlugins( MR::Viewer::instance() );
-    viewerSetup.setupCommonModifiers( MR::Viewer::instance() );
-    viewerSetup.setupCommonPlugins( MR::Viewer::instance() );
-    viewerSetup.setupSettingsManager( MR::Viewer::instance(), launchParams.name );
-    viewerSetup.setupConfiguration( MR::Viewer::instance() );
-    viewerSetup.setupExtendedLibraries();
+	MR::ViewerSetup viewerSetup;
+	viewerSetup.setupBasePlugins(MR::Viewer::instance());
+	viewerSetup.setupCommonModifiers(MR::Viewer::instance());
+	viewerSetup.setupCommonPlugins(MR::Viewer::instance());
+	viewerSetup.setupSettingsManager(MR::Viewer::instance(), launchParams.name);
+	viewerSetup.setupConfiguration(MR::Viewer::instance());
 
-        return MR::launchDefaultViewer(launchParams, viewerSetup);
+	return MR::launchDefaultViewer(launchParams, viewerSetup);
 }
